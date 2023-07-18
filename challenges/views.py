370159d55805e1a48d42c49cb57bc519bdad01ce
Sushovit204 <shahsushovit434@gmail.com>
2023-07-18 17:38:47 +0545

    variable Interpolation

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
 
 
 monthly_challenges={
@@ -48,8 +47,10 @@ def monthly_challenge(request, month):
     challenge_text = None
     try:
         challenge_text=monthly_challenges[month]
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month.capitalize()
+        })
     except:
         return HttpResponseNotFound(f"<h1>This is not a valid month.</h1>")
     
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
-Install python and pip manager
-Run pip install -r requirements.txt
-Run server using "python manage.py runserver" command
+## Installation
+    1. Install python and pip manager
+    2. Run pip install -r requirements.txt
+
+##Usage
+    1.Run server using "python manage.py runserver" command
