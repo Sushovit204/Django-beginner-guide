689b102bd2f0d26db8d2bf547d1964fb4dcd503b
Sushovit204 <shahsushovit434@gmail.com>
2023-07-22 23:17:35 +0545

    Finished the lesson

--- mypage/urls.py
+++ mypage/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from landing_page import views
 
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('challenges/', include("challenges.urls"))
 ]
